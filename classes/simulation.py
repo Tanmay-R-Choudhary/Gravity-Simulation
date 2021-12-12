7c240b4e0691925ae756fa6800e361f9ff66ff4e
Tanmay-R-Choudhary <tanmay.d.choudhary@gmail.com>
2021-12-12 18:58:39 +0530

    First commit

---
+++ classes/simulation.py
@@ -0,0 +1,46 @@
+import pygame
+import numpy as np
+import time
+from src.classes.physics_engine import PhysicsEngine
+
+
+class Simulation:
+    physics_engine = PhysicsEngine()
+
+    def __init__(self):
+        self.run, self.space, self.bodies = None, None, None
+
+    def initialise_environment(self, *args):
+        self.bodies = [i for i in args]
+        space_plane_size = (1000, 800)  # width, height of canvas
+        self.run = True
+
+        # setting up pygame
+        pygame.init()
+        pygame.display.set_caption("Orbit simulator")
+        self.space = pygame.display.set_mode(space_plane_size)
+
+        # setting up physics engine
+        self.physics_engine.define_bodies(args)
+
+    def show_environment(self):
+        while self.run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run = False
+
+            self.space.fill((0, 0, 0))
+
+            net_force = self.physics_engine.compute_force_vectors()
+            for i, body in enumerate(self.bodies):
+                body.force = net_force[i]
+
+            for body in self.bodies:
+                body.draw(self.space)
+
+            for body in self.bodies:
+                body.move()
+                # print(body.velocity)
+                # print(body.position)
+            time.sleep(0.0005)
+            pygame.display.update()
---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+from src.classes.body import Body
+from src.classes.simulation import Simulation
+
+
+body1 = Body(np.array([[500, 300, 0]]), 6 * pow(10, 15), (255, 255, 255))
+body1.add_velocity(np.array([[40, 0, 0]]))
+
+body2 = Body(np.array([[600, 200, 0]]), 6 * pow(10, 15), (0, 255, 0))
+body2.add_velocity(np.array([[-40, 0, 0]]))
+
+body3 = Body(np.array([[500, 500, 0]]), 6 * pow(10, 15), (255, 0, 0))
+body3.add_velocity(np.array([[50, 0, 0]]))
+
+sim = Simulation()
+sim.initialise_environment(body1, body2, body3)
+sim.show_environment()
