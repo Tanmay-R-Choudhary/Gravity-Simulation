f90967a1f39f3dc0367482edb339dc1420e157df
Tanmay-R-Choudhary <tanmay.d.choudhary@gmail.com>
2021-12-14 12:40:29 +0530

    Added scenarios

--- main.py
+++ main.py
@@ -3,15 +3,56 @@ from src.classes.body import Body
 from src.classes.simulation import Simulation
 
 
+# erratic orbits that result in one body off-screen and two bodies in stable binary system that also moves off screen
+# body1 = Body(np.array([[500, 300, 0]]), 6 * pow(10, 15), (255, 255, 255))
+# body1.add_velocity(np.array([[40, 0, 0]]))
+#
+# body2 = Body(np.array([[600, 200, 0]]), 6 * pow(10, 15), (0, 0, 255))
+# body2.add_velocity(np.array([[-40, 0, 0]]))
+#
+# body3 = Body(np.array([[300, 500, 0]]), 6 * pow(10, 15), (0, 255, 0))
+# body3.add_velocity(np.array([[50, 0, 0]]))
+
+# ------------------------------------#
+
+# stable circular orbit
+# body1 = Body(np.array([[500, 400, 0]]), 6 * pow(10, 15), (255, 255, 255))
+#
+# body2 = Body(np.array([[500, 600, 0]]), 6 * pow(10, 9), (0, 0, 255))
+# body2.add_velocity(np.array([[200, 0, 0]]))
+
+# ------------------------------------#
+
+# stable elliptical orbit
+# body1 = Body(np.array([[500, 400, 0]]), 6 * pow(10, 15), (255, 255, 255))
+#
+# body2 = Body(np.array([[500, 500, 0]]), 6 * pow(10, 9), (0, 0, 255))
+# body2.add_velocity(np.array([[200, 0, 0]]))
+
+# ------------------------------------#
+
+# stable binary star system
+# body1 = Body(np.array([[500, 300, 0]]), 6 * pow(10, 15), (255, 255, 255))
+# body1.add_velocity(np.array([[-100, 0, 0]]))
+#
+# body2 = Body(np.array([[500, 500, 0]]), 6 * pow(10, 15), (0, 0, 255))
+# body2.add_velocity(np.array([[100, 0, 0]]))
+
+# ------------------------------------#
+
+# I don't know if this exists. Looks good though!
 body1 = Body(np.array([[500, 300, 0]]), 6 * pow(10, 15), (255, 255, 255))
-body1.add_velocity(np.array([[40, 0, 0]]))
+body1.add_velocity(np.array([[-100, 0, 0]]))
 
-body2 = Body(np.array([[600, 200, 0]]), 6 * pow(10, 15), (0, 255, 0))
-body2.add_velocity(np.array([[-40, 0, 0]]))
+body2 = Body(np.array([[400, 400, 0]]), 6 * pow(10, 15), (0, 0, 255))
+body2.add_velocity(np.array([[0, 100, 0]]))
 
-body3 = Body(np.array([[300, 500, 0]]), 6 * pow(10, 15), (255, 0, 0))
-body3.add_velocity(np.array([[50, 0, 0]]))
+body3 = Body(np.array([[500, 500, 0]]), 6 * pow(10, 15), (0, 255, 0))
+body3.add_velocity(np.array([[100, 0, 0]]))
+
+body4 = Body(np.array([[600, 400, 0]]), 6 * pow(10, 15), (255, 0, 0))
+body4.add_velocity(np.array([[0, -100, 0]]))
 
 sim = Simulation()
-sim.initialise_environment(body1, body2, body3)
+sim.initialise_environment(body1, body2, body3, body4)
 sim.show_environment()
